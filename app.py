766c66230abe3bbb6f69a4761d3d8d4a667471e1
sourimmitramol <sourim.mitra@molgroup.com>
2025-07-05 02:03:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def ask():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-#if __name__ == '__main__':
-#    app.run(host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000)
         
